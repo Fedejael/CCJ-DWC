3a10122f566ab4eb193f187e6a1574725772b268
fedejael <federicojael.canto@gmail.com>
2025-09-04 19:32:53 -0300

    Variables en JavaScript

---
+++ INTRODUCCIONJS/js/01.js
@@ -0,0 +1,26 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor 
+
+var disponible; // Iniciamos la variable pero sin valor.
+
+/* producto = true; */ // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categorias = 'Computadoras';
+
+
+/* var 1disponible; */
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // camelCase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'MonitorHD';
+
+
+console.log(PRODUCTO)
