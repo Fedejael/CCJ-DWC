a412103c473a74108ff66de9aea9f249b3b97278
fedejael <federicojael.canto@gmail.com>
2025-09-05 10:54:13 -0300

    Variables con let y const

---
+++ INTRODUCCIONJS/js/02.js
@@ -0,0 +1,24 @@
+// Variables con let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor 
+
+let disponible; // Iniciamos la variable pero sin valor.
+
+/* producto = true; */ // Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categorias = 'Computadoras';
+
+
+/* let 1disponible; */
+let disponible_;
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // camelCase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'MonitorHD';
+
