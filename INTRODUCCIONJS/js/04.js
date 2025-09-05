22f816a9fe6020aad63e54c0c5f3b6a5e394e037
fedejael <federicojael.canto@gmail.com>
2025-09-05 12:29:22 -0300

    Strings o Cadenas de Texto

---
+++ INTRODUCCIONJS/js/04.js
@@ -0,0 +1,10 @@
+// String o Cadenas de Texto
+
+const producto = 'Monitor de 20 Pulgadas';
+/* const producto2 = String('Monitor 30 Pulgadas');
+const producto3 = new String('Monitor 50 Pulgadas'); */
+
+
+console.log(producto);
+/* console.log(producto2);
+console.log(producto3); */
