f0226d5fbf239581f4c3fe0e4c9d50fa9c361faa
fedejael <federicojael.canto@gmail.com>
2025-09-05 20:19:49 -0300

    Concatenación y Template Strings

---
+++ INTRODUCCIONJS/js/08.js
@@ -0,0 +1,13 @@
+const nombre = 'Federico';
+const email = 'correo@correo.com'
+
+
+console.log(nombre);
+console.log(email);
+
+// Concatenación
+console.log("Nombre del cliente: " + nombre + " - Email del cliente: " + email);
+
+
+// Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
