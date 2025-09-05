82ac06b66f6905bf19ecf0f80fe8b31dca81c086
fedejael <federicojael.canto@gmail.com>
2025-09-05 12:47:42 -0300

    Métodos para los Strings

--- INTRODUCCIONJS/js/04.js
+++ INTRODUCCIONJS/js/04.js
@@ -1,10 +1,23 @@
 // String o Cadenas de Texto
 
-const producto = 'Monitor de 20 Pulgadas';
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo';
+const producto2 = 'Monitor HD';
+const email = 'correo@correo.com'
 /* const producto2 = String('Monitor 30 Pulgadas');
 const producto3 = new String('Monitor 50 Pulgadas'); */
 
-
-console.log(producto);
+// length es para la extension (cuenta el total de caracteres dentro del string)
+console.log(tweet.length);
+console.log(producto2);
 /* console.log(producto2);
 console.log(producto3); */
+
+// IndexOf (retorna posición)
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'))
+console.log(email.indexOf('@'));
+
+// Includes (retorna true o false )
+
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('Tablet'))
