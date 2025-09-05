6fd073c854907243c2d2f177fae9b1f6d840507e
fedejael <federicojael.canto@gmail.com>
2025-09-05 20:36:44 -0300

    Objetos

---
+++ INTRODUCCIONJS/js/10.js
@@ -0,0 +1,17 @@
+// Objetos
+
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+
+console.log(producto)
+
+console.log(producto.precio);
+console.log(producto.nombreProducto);
+console.log(producto.disponible);
+
+
+console.log(producto["precio"]);
