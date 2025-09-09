62cdf1a41f1ce3b1be32db73cb7fb97e87ccadf0
fedejael <federicojael.canto@gmail.com>
2025-09-08 23:58:21 -0300

    For Loops

---
+++ INTRODUCCIONJS/js/24.js
@@ -0,0 +1,19 @@
+// For Loop
+
+/* for( let i = 0 ; i <= 10; i++ ) {
+    console.log(i);
+} */
+
+for( let i = 1 ; i <= 100; i++ ) {
+    if (i % 2 === 0) {
+        console.log(`El Número ${i} es PAR`);
+    } else {
+        console.log(`El Número ${i} es IMPAR`);
+    }
+}
+
+// While Loop
+
+
+
+// Do While Loop
