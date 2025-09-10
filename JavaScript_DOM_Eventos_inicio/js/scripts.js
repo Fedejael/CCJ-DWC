19be9a821fa35ae882ed16b0ebf6e5bcfaa57d65
fedejael <federicojael.canto@gmail.com>
2025-09-10 11:04:07 -0300

    Seleccionar Elementos con querySelector

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,10 @@
+// querySelector
+
+const heading = document.querySelector('#heading') // Retorna 0 o 1 elemento
+heading.textContent = "HOLA MUNDO";
+console.log(heading)
+
+// querySelectorAll
+
+
+// getElmentByID
