c741f02b30f24c790049363d60b89b8c8200fc9a
fedejael <federicojael.canto@gmail.com>
2025-09-10 11:28:15 -0300

    Seleccionar Elementos con querySelectorAll

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -6,5 +6,12 @@ console.log(heading)
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = "Nuevo Texto para Enlace";
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 // getElmentByID
+
+const heading2 =document.getElementById('heading');
+console.log(heading2);
