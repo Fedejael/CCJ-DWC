18f4e1a84914bcce80ffd7d8cb7ecb27b7ff0ef9
fedejael <federicojael.canto@gmail.com>
2025-09-09 23:17:25 -0300

    Fetch API

---
+++ INTRODUCCIONJS/js/33.js
@@ -0,0 +1,13 @@
+async function obtenerEmpleados() {
+
+    const archivo = 'empleados.json';
+
+/*     fetch(archivo)
+        .then( resultado => resultado.json())
+        .then ( datos => console.log(datos)); */
+
+    const resultado = await fetch(archivo);
+    const datos = await resultado.json();
+    console.log(datos);
+}
+obtenerEmpleados();
