924edcb674bfbc7f35551ca7fd150f27f9e07aac
fedejael <federicojael.canto@gmail.com>
2025-09-10 12:18:31 -0300

    Eventos en JavaScript

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -9,9 +9,26 @@ console.log(heading)
 const enlaces = document.querySelectorAll('.navegacion a');
 enlaces[0].textContent = "Nuevo Texto para Enlace";
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+/* enlaces[0].classList.remove('navegacion__enlace'); */
 
-// getElmentByID
+// getElmentByID - FORMA VIEJA
 
-const heading2 =document.getElementById('heading');
-console.log(heading2);
+/* const heading2 =document.getElementById('heading');
+console.log(heading2); */
+
+// Generar un nuevo enlace
+
+const nuevoEnlace = document.createElement('A')
+
+// Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+// Agregar el texto
+nuevoEnlace.textContent = 'Tienda Virtual';
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+console.log(nuevoEnlace);
